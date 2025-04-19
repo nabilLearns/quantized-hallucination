4abbd815aa66af95a51a0695cb61399803c99c01
nabilLearns <n.mohamed135@gmail.com>
2025-04-19 11:36:38 +0000

    add results & plots for biomistral-7B-med

--- do_all_exps.py
+++ do_all_exps.py
@@ -3,16 +3,21 @@ import time
 import pandas
 import os
 #extra_models = ['qwen2.5-32b-it','qwen2.5-7b-it-med',]
-models = ['biomistral-7b-med',
-          'gemma3-1b-it',
-          'gemma3-4b-it',
-          'gemma3-12b-it',
-          'llama3-med',
-          'qwen2.5-0.5b-it',
-          'qwen2.5-1.5b-it',
-          'qwen2.5-3b-it',
-          'qwen2.5-7b-it',
-          'qwen2.5-14b-it']
+models = [
+        'biomistral-7b-med',
+        
+        #'gemma3-1b-it',
+        #'gemma3-4b-it',
+        
+        #'gemma3-12b-it',
+        #'llama3-med',
+        
+        #'qwen2.5-0.5b-it',
+        #'qwen2.5-1.5b-it',
+        'qwen2.5-3b-it',
+          #'qwen2.5-7b-it',
+          #'qwen2.5-14b-it'
+          ]
 gguf_quants = ['q8_0', 'q6_k', 'q5_k_m', 'q4_k_m', 'q3_k_m', 'q2_k']
 
 # example cli input for one run
@@ -38,4 +43,4 @@ for model in models:
         print(f"✅ Finished {model} @ {quant} in {duration:.2f} seconds\n")
         #subprocess.call(['python', 'run-llm-exp.py', 'model', model, 'quantization', 'gguf', 'quantization.level', quant])
 exp_runtimes = pd.DataFrame(exp_runtimes)
-exp_runtimes.to_csv('exp_runtimes.csv')
+exp_runtimes.to_csv('exp_runtimes_A40_93.csv')
